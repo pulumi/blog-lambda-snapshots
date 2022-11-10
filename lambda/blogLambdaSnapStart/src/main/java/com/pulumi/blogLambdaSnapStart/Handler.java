1981ff83d61ff3c3615323cc4064781c84230fa2
Piers Karsenbarg <piers@pulumi.com>
2022-11-10 14:41:10 +0000

    added apigatewayv2

--- lambda/blogLambdaSnapStart/src/main/java/com/pulumi/blogLambdaSnapStart/Handler.java
+++ lambda/blogLambdaSnapStart/src/main/java/com/pulumi/blogLambdaSnapStart/Handler.java
@@ -14,15 +14,15 @@ public class Handler implements RequestHandler<Map<String, String>, String> {
 
     @Override
     public String handleRequest(Map<String, String> event, Context context) {
-        LambdaLogger logger = context.getLogger();
-        String response = "200 OK";
+        //LambdaLogger logger = context.getLogger();
+        String response = new String("{\"statusCode\": 200, body: \"Hello World\"}");
         // log execution details
-        logger.log(("Hello world!"));
-        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
-        logger.log("CONTEXT: " + gson.toJson(context));
-        // process event
-        logger.log("EVENT: " + gson.toJson(event));
-        logger.log("EVENT TYPE: " + event.getClass());
+        // logger.log(("Hello world!"));
+        // logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        // logger.log("CONTEXT: " + gson.toJson(context));
+        // // process event
+        // logger.log("EVENT: " + gson.toJson(event));
+        // logger.log("EVENT TYPE: " + event.getClass());
         return response;
     }
 }
--- pulumi/no-snapstart/index.ts
+++ pulumi/no-snapstart/index.ts
@@ -1,23 +1,29 @@
 import * as awsNative from "@pulumi/aws-native";
 import * as aws from "@pulumi/aws";
 import * as command from "@pulumi/command";
+import * as pulumi from "@pulumi/pulumi";
+
+// const role = new aws.iam.Role("role", {
+//   assumeRolePolicy: JSON.stringify({
+//     "Version": "2012-10-17",
+//     "Statement": [{
+//       "Effect": "Allow",
+//       "Principal": {
+//         "Service": "lambda.amazonaws.com",
+//       },
+//       "Action": "sts:AssumeRole",
+//     }],
+//   }),
+// });
+
+// new aws.iam.RolePolicyAttachment("role-policy-attachment", {
+//   role: role.name,
+//   policyArn: "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+// });
 
 const role = new aws.iam.Role("role", {
-  assumeRolePolicy: JSON.stringify({
-    "Version": "2012-10-17",
-    "Statement": [{
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com",
-      },
-      "Action": "sts:AssumeRole",
-    }],
-  }),
-});
-
-new aws.iam.RolePolicyAttachment("role-policy-attachment", {
-  role: role.name,
-  policyArn: "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(aws.iam.Principals.LambdaPrincipal),
+  managedPolicyArns: [aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole]
 });
 
 const mvnOutputs = command.local.runOutput({
@@ -48,6 +54,32 @@ const func = new awsNative.lambda.Function("no-snapstart-func", {
   handler: "com.pulumi.blogLambdaSnapStart.Handler",
   timeout: 30,
 });
+
+// const classicFunc = new aws.lambda.Function("lambda", {
+//   role: role.arn,
+//   s3Bucket: functionCode.bucket,
+//   s3Key: functionCode.key,
+//   s3ObjectVersion: functionCode.versionId,
+//   runtime: "java11",
+//   handler: "com.pulumi.blogLambdaSnapStart.Handler",
+//   timeout: 30,
+// })
+
+// export const invokearn = classicFunc.invokeArn;
+
+const api = new aws.apigatewayv2.Api("api", {
+    protocolType: "HTTP",
+    routeKey: "GET /",
+    target: pulumi.interpolate`arn:aws:apigateway:eu-west-1:lambda:path/2015-03-31/functions/${func.arn}/invocations`
+})
+
+const perm2 = new aws.lambda.Permission("permission", {
+  action: "lambda:InvokeFunction",
+  principal: "apigateway.amazonaws.com",
+  function: func.arn
+})
+
+export const apiurl = api.apiEndpoint;
 
 
 // Gives our alias URL the necessary perms to be invoked without authentication"
